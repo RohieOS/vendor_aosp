a6417c5cf2ba7185d71a802f0a12572a884a970c
Manish4586 <manish.n.manish45@gmail.com>
2021-02-01 02:06:54 +0530

    Revert "vendor: add ota"
    This reverts commit d6e6c8939a682bd16344febda30db4d3415690ea.

--- config/packages.mk
+++ config/packages.mk
@@ -43,8 +43,3 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 endif
 
-ifneq ($(filter OFFICIAL,$(ROHIE_BUILD_TYPE)),)
-PRODUCT_PACKAGES += \
-    Updates
-endif
-
