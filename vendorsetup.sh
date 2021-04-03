f9d6fcaaf3735a615e32dd43cce9e0370a8aee1c
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-04-03 14:11:10 +0530

    versioning: get CAF Tag reliably
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,3 @@
 # SDClang Environment Variables
 export SDCLANG_CONFIG=$(pwd)/vendor/aosp/sdclang/sdclang.json
+export CAF_REVISION_TAG=$(grep "revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
