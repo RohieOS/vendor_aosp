059e0f6c850c4f1813ce885e8cb336d5558ebb76
jhenrique09 <jhsv09@gmail.com>
2021-10-11 13:37:20 -0300

    vendor: Remove deprecated overlays
    Change-Id: I689a6382770df90debc80014746750a4be7519c3
    Signed-off-by: manish4586 <manish.n.manish45@gmail.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -1,18 +1,5 @@
 # RRO Overlays
 PRODUCT_PACKAGES += \
     PixelSetupWizardAodOverlay \
-    GooglePermissionControllerOverlay \
-    PixelDocumentsUIGoogleOverlay \
-    IconPackCircularPixelLauncherOverlay \
-    IconPackCircularPixelThemePickerOverlay \
-    IconPackFilledPixelLauncherOverlay \
-    IconPackFilledPixelThemePickerOverlay \
-    IconPackRoundedPixelLauncherOverlay \
-    IconPackRoundedPixelThemePickerOverlay \
-    FontArbutusSourceOverlay \
-    FontArvoLatoOverlay \
-    FontKaiOverlay \
-    FontRubikRubikOverlay \
-    FontSamOverlay \
-    FontVictorOverlay
+    PixelDocumentsUIGoogleOverlay
 
